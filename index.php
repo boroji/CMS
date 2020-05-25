db64c6b17e38e3f17feac9bce544ea4a64ff3a22
Hossein <hosseinboroji@gmail.com>
2020-05-25 14:12:50 -0400

    added dynamic post title, author, content, date

--- index.php
+++ index.php
@@ -11,6 +11,20 @@
         <!-- Blog Entries Column -->
         <div class="col-md-8">
 
+            <?php
+
+            $query = "SELECT * FROM posts";
+            $result_post = mysqli_query($connection, $query);
+
+            while ($row_post = mysqli_fetch_assoc($result_post)) {
+                $post_title = $row_post['post_title'];
+                $post_author = $row_post['post_author'];
+                $post_date = $row_post['post_date'];
+                $post_content = $row_post['post_content'];
+                $post_image = $row_post['post_image'];
+
+            ?>
+
             <h1 class="page-header">
                 Page Heading
                 <small>Secondary Text</small>
@@ -18,19 +32,21 @@
 
             <!-- First Blog Post -->
             <h2>
-                <a href="#">Blog Post Title</a>
+                <a href="#"><?php echo $post_title; ?></a>
             </h2>
             <p class="lead">
-                by <a href="index.php">Start Bootstrap</a>
+                by <a href="index.php"><?php echo $post_author; ?></a>
             </p>
-            <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+            <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
             <hr>
             <img class="img-responsive" src="http://placehold.it/900x300" alt="">
             <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+            <p><?php echo $post_content; ?></p>
             <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
             <hr>
+
+            <?php } ?>
 
         </div>
 
