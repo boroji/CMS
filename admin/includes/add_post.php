bf42d39dd587a9dcb109837ae89b547219b6a3c6
Hossein <hosseinboroji@gmail.com>
2020-05-27 19:01:06 -0400

    adding post category to add_post section

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 
 if (isset($_POST['create_post'])) {
     $post_title = $_POST['title'];
     $post_author  = $_POST['author'];
-    $post_category_id = $_POST['post_category_id'];
+    $post_category_id = $_POST['post_category'];
     $post_status = $_POST['post_status'];
 
     $post_image = $_FILES['image']['name'];
@@ -24,48 +24,75 @@ if (isset($_POST['create_post'])) {
 }
 
 ?>
- 
- <form action="" method="post" enctype="multipart/form-data">
 
-     <div class="form-group">
-         <label for="title">Title</label>
-         <input type="text" class="form-control" name="title">
-     </div>
+<form action="" method="post" enctype="multipart/form-data">
 
-     <div class="form-group">
-         <label for="post_category_id">Category</label>
-         <input type="text" class="form-control" name="post_category_id">
-     </div>
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" class="form-control" name="title">
+    </div>
 
-     <div class="form-group">
-         <label for="author">Author</label>
-         <input type="text" class="form-control" name="author">
-     </div>
+    <div class="form-group">
+        <label for="categories">Categories</label>
+        <select name="post_category" id="post_category">
 
-     <div class="form-group">
-         <label for="post_status">Status</label>
-         <input type="text" class="form-control" name="post_status">
-     </div>
+            <?php
 
-     <div class="form-group">
-         <label for="post_image">Image</label>
-         <input type="file" name="image">
-     </div>
+            $query = "SELECT * FROM categories ";
+            $select_categories = mysqli_query($connection, $query);
 
-     <div class="form-group">
-         <label for="post_tags">Tags</label>
-         <input type="text" class="form-control" name="post_tags">
-     </div>
 
-     <div class="form-group">
-         <label for="post_content">Post Content</label>
-         <textarea class="form-control " name="post_content" id="" cols="30" rows="10">
+            while ($row = mysqli_fetch_assoc($select_categories)) {
+                $cat_id = $row['cat_id'];
+                $cat_title = $row['cat_title'];
+
+
+                if ($cat_id == $post_category_id) {
+
+
+                    echo "<option selected value='{$cat_id}'>{$cat_title}</option>";
+                } else {
+
+                    echo "<option value='{$cat_id}'>{$cat_title}</option>";
+                }
+            }
+
+            ?>
+
+
+        </select>
+
+    </div>
+
+    <div class="form-group">
+        <label for="author">Author</label>
+        <input type="text" class="form-control" name="author">
+    </div>
+
+    <div class="form-group">
+        <label for="post_status">Status</label>
+        <input type="text" class="form-control" name="post_status">
+    </div>
+
+    <div class="form-group">
+        <label for="post_image">Image</label>
+        <input type="file" name="image">
+    </div>
+
+    <div class="form-group">
+        <label for="post_tags">Tags</label>
+        <input type="text" class="form-control" name="post_tags">
+    </div>
+
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class="form-control " name="post_content" id="" cols="30" rows="10">
          </textarea>
-     </div>
+    </div>
 
-     <div class="form-group">
-         <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
-     </div>
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+    </div>
 
 
- </form>
+</form>
